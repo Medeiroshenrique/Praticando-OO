68b808145827c6a8d0f5310d79ec44c790e097f2
Henrique Medeiros <henrique.c.r.medeiros@gmail.com>
2022-04-13 23:50:28 -0300

    Brincando com OO

---
+++ InformacaoPessoa.cs
@@ -0,0 +1,24 @@
+﻿class InformacaoPessoa
+    {
+    private string _nome;
+    public int Idade;
+    private string _numeroInscricao;
+
+    public void setInscricao(string numeroInscricao){
+        _numeroInscricao=numeroInscricao;
+        }
+    public string getInscricao() {
+        return _numeroInscricao;
+    }
+    public void setNome(string Nome) {
+        _nome = Nome;
+    }
+    public string getNome()
+    {
+        return _nome;
+    }
+    }
+
+
+    
+
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao EleitorAll!");
+            InformacaoPessoa p = new InformacaoPessoa();
+            p.setNome("Anakin Skywalker");
+            Console.WriteLine("Digite seu numero de inscricao:");
+            p.setInscricao(Console.ReadLine());
+            Console.WriteLine("Digite sua idade:");
+            p.Idade=int.Parse(Console.ReadLine());
+            Console.WriteLine($"Nome: {p.getNome()}");
+            Console.WriteLine($"N° de inscricao: {p.getInscricao()}");
+            Console.WriteLine($"Idade: {p.Idade} anos");
+        }
+    }
+}
